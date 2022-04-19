8d1b56d01d4000c656d80baf3536b3d588b10dee
Kai Wang <kaiuchihaa@gmail.com>
2022-04-18 21:45:26 -0300

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from './src/config/expressCustom.js';
 import 'dotenv/config';
 
-const port = process.env.PORT|| 3001;
+const port = process.env.PORT|| 3030;
 
 app.listen(port, ()=>{
     console.log(`logado com sucesso em http://localhost:${port}`)
