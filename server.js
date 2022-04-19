d384a183ae6cc1891fea5e50cb1e4e29050ba644
Kai Wang <kaiuchihaa@gmail.com>
2022-04-18 21:42:11 -0300

    heroku

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ import 'dotenv/config';
 
 const port = process.env.PORT|| 3001;
 
-let server =  app.listen(port, ()=>{
+app.listen(port, ()=>{
     console.log(`logado com sucesso em http://localhost:${port}`)
 })
-
-server.on('clientError', (err, socket) => {
-  console.error(err);
-  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-});
